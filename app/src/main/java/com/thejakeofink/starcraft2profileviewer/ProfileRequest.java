fbd41f76beddab210e4ec4ce0c19cf140e0dd2f0
Jake Stokes <jstokes@sorenson.com>
2014-09-08 22:55:02 -0600

    Initial Commit.

---
+++ app/src/main/java/com/thejakeofink/starcraft2profileviewer/ProfileRequest.java
@@ -0,0 +1,14 @@
+package com.thejakeofink.starcraft2profileviewer;
+
+/**
+ * Created by Jacob Stokes on 9/8/14.
+ */
+public class ProfileRequest {
+    String url;
+
+    public ProfileRequest(String url) {
+        this.url = url;
+    }
+
+    public
+}
