cc9a28f2e1a5506c99699254e9e0fa0dfe457ee6
Jake Stokes <jstokes@sorenson.com>
2014-09-08 23:33:17 -0600

    Added profile response and url connector for work with api and JSON.

--- app/src/main/java/com/thejakeofink/starcraft2profileviewer/ProfileRequest.java
+++ app/src/main/java/com/thejakeofink/starcraft2profileviewer/ProfileRequest.java
@@ -1,14 +1,53 @@
 package com.thejakeofink.starcraft2profileviewer;
 
+import java.io.ByteArrayOutputStream;
+
 /**
  * Created by Jacob Stokes on 9/8/14.
  */
 public class ProfileRequest {
-    String url;
+    private static final String region = "http://us.battle.net/api/sc2/profile/";
 
-    public ProfileRequest(String url) {
-        this.url = url;
+    String userName;
+    String accountNumber;
+    ProfileResponse response;
+
+    /*
+    Constructor for request, takes in the users profile string
+     */
+    public ProfileRequest(String userName, String accountNumber) {
+        this.userName = userName;
+        this.accountNumber = accountNumber;
     }
 
-    public
+    /*
+    Allows us to get our response after it has come through.
+     */
+    public ProfileResponse getResponse() {
+        return response;
+    }
+
+    /*
+    Sends request to API
+     */
+    public void sendRequest() {
+        Thread sendRequestToAPI = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                downloadAndLoadJSON();
+            }
+        });
+        sendRequestToAPI.start();
+    }
+
+    /*
+    Builds string for request URL.
+     */
+    private String buildURLForRequest() {
+        return region + accountNumber + "/1/" + userName + "/";
+    }
+
+    private void downloadAndLoadJSON() {
+        ByteArrayOutputStream baos = URLConnector.readBytes(buildURLForRequest());
+    }
 }
---
+++ app/src/main/java/com/thejakeofink/starcraft2profileviewer/ProfileResponse.java
@@ -0,0 +1,14 @@
+package com.thejakeofink.starcraft2profileviewer;
+
+import org.json.JSONObject;
+
+/**
+ * Created by Jacob Stokes on 9/8/14.
+ */
+public class ProfileResponse {
+    JSONObject responseObject;
+
+    public JSONObject getResponse() {
+        return responseObject;
+    }
+}
